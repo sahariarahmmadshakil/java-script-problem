a27b283c4bee20ef0863589f02c1cd281fdb1bf5
sahariarahmmadshakil <info.sashakil@gmail.com>
2022-08-11 01:40:25 +0600

    second problem

--- problems.js
+++ problems.js
@@ -1,14 +1,27 @@
 // First Problem radianToDegree 
 
 
-function radianToDegree(radian)
-{
-  const pi = 3.14159;
-  const degree = radian * (180/pi);
-  const output = degree.toFixed(2);
-  return (output);
-}
+// function radianToDegree(radian)
+// {
+//   const pi = 3.14159;
+//   const degree = radian * (180/pi);
+//   const output = degree.toFixed(2);
+//   return (output);
+// }
           
-console.log(radianToDegree(10));
-
 // ====================================
+
+// Problem Number 2
+
+function isJavaScriptFile(name){
+const last3 = name.slice(-3);
+if (last3==".js"){
+    return true;
+}else{
+    return false;
+}
+}
+const output=isJavaScriptFile("Shakil.m");
+console.log(output);
+
+// ==========================
